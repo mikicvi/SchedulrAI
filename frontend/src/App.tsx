1892607ac0f51ffb401da41eb2f753fceb3f3cd3
mikicvi <88291034+mikicvi@users.noreply.github.com>
2024-11-25 17:20:24 +0000

    feat: consume backend API's, demo interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,42 +19,41 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import DynamicTextarea from './components/ui/textarea-prompt';
 import { Button } from '@/components/ui/button';
+import { LoadingSpinner } from '@/components/ui/loading-spinner';
 import { Link } from 'react-router-dom';
-
-import { useEffect, useState } from 'react';
-
-// Hook to check the status of the backend, which is exposed via REST API at /api/checkPipelineStatus
-function useBackendStatus() {
-	const [status, setStatus] = useState('Not Ready');
-
-	useEffect(() => {
-		async function fetchStatus() {
-			try {
-				const response = await fetch('http://localhost:3000/api/checkPipelineStatus');
-				if (response.ok) {
-					const data = await response.json();
-					// check if data has ready: true
-					if (!data.ready) {
-						setStatus('🟡 Loading: Backend is still initializing');
-						return;
-					}
-					setStatus('🟢 Ready: All systems reporting healthy');
-				} else {
-					setStatus('🔴 Error in backend');
-				}
-			} catch (error) {
-				setStatus('🔴 Error in backend');
-			}
-		}
-		fetchStatus();
-	}, []);
-
-	return status;
-}
+import useBackendStatus from './hooks/use-backend';
+import { useState } from 'react';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './components/ui/card';
 
 function App() {
+	const [userInput, setUserInput] = useState('');
+	const [loading, setLoading] = useState(false);
+	const [response, setResponse] = useState(null);
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setUserInput(event.target.value);
+	};
+
+	const handleSubmit = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch('http://localhost:3000/api/runPipeline', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ userInput: userInput }),
+			});
+			const data = await response.json();
+			setResponse(data);
+		} catch (error) {
+			console.error('Error:', error);
+		} finally {
+			setLoading(false);
+		}
+	};
 	return (
-		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+		<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
 			<div className="flex flex-col h-full">
 				<div className="flex justify-between items-start p-4">
 					<DropdownMenu>
@@ -86,26 +85,45 @@ function App() {
 							</BreadcrumbItem>
 							<BreadcrumbSeparator />
 							<BreadcrumbItem>
-								<BreadcrumbLink href="/components">Components</BreadcrumbLink>
-							</BreadcrumbItem>
-							<BreadcrumbSeparator />
-							<BreadcrumbItem>
-								<BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+								<BreadcrumbPage>Task Time Estimation</BreadcrumbPage>
 							</BreadcrumbItem>
 						</BreadcrumbList>
 					</Breadcrumb>
 
 					<ModeToggle />
 				</div>
-				<div className="flex justify-center items-center h-screen">
+				<div className="flex justify-center items-center pt-[25vh]">
 					<DynamicTextarea
 						label={useBackendStatus()}
 						placeholder="Type/paste customer requirements here"
 						id="message"
+						value={userInput}
+						onChange={handleInputChange}
 					/>
 					<div className="h-4 px-4">
-						<Button variant="outline">Submit</Button>
+						{loading ? (
+							<LoadingSpinner className="ml-4" />
+						) : (
+							<Button variant="outline" onClick={handleSubmit} disabled={loading}>
+								Submit
+							</Button>
+						)}
 					</div>
+				</div>
+				<div className="flex justify-center items-center text-white pt-4">
+					{response && (
+						<Card>
+							<CardHeader>
+								<CardTitle>Time</CardTitle>
+								<CardDescription>Estimated time to complete the task via RAG pipeline</CardDescription>
+							</CardHeader>
+							<CardContent>
+								<Button variant="destructive">
+									<pre>{JSON.stringify(response.result, null, 2).replace(/\"/g, '')}</pre>
+								</Button>
+							</CardContent>
+						</Card>
+					)}
 				</div>
 			</div>
 		</ThemeProvider>
---
+++ frontend/src/hooks/use-backend.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+// Hook to check the status of the backend, which is exposed via REST API at /api/checkPipelineStatus
+function useBackendStatus() {
+	const [status, setStatus] = useState('Not Ready');
+
+	useEffect(() => {
+		async function fetchStatus() {
+			try {
+				const response = await fetch('http://localhost:3000/api/checkPipelineStatus');
+				if (response.ok) {
+					const data = await response.json();
+					// check if data has ready: true
+					if (!data.ready) {
+						setStatus('🟡 Loading: Backend is still initializing');
+						return;
+					}
+					setStatus('🟢 Ready: All systems reporting healthy');
+				} else {
+					setStatus('🔴 Error in backend');
+					setTimeout(fetchStatus, 5000); // Poll every 5 seconds if error
+				}
+			} catch (error) {
+				setStatus('🔴 Error in backend');
+				setTimeout(fetchStatus, 5000); // Poll every 5 seconds if error
+			}
+		}
+		fetchStatus();
+	}, []);
+
+	return status;
+}
+
+export default useBackendStatus;

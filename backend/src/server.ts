237caa8e66dec2ac9f5596ef1fa5c9f49e1054e7
mikicvi <88291034+mikicvi@users.noreply.github.com>
2024-11-25 14:59:38 +0000

    server cleanup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,8 @@
 import app from './app';
-import RAGPipeline from './services/pipelineServices';
 
 const port = process.env.EXPRESS_PORT || 3000;
 const proto = process.env.PROTOCOL || 'http';
 
 app.listen(port, () => {
-	console.log(`Express is listening at ${process.env.PROTOCOL}://localhost:${port}`);
+	console.log(`Express is listening at ${proto}://localhost:${port}`);
 });

44c35776e22ac6d8f77032c9cf610de5a8a1d6b0
mikicvi <88291034+mikicvi@users.noreply.github.com>
2025-01-23 12:05:16 +0000

    feat: set up unit tests via Jest

---
+++ backend/jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/tests/'],
+};

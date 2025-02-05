be03a8713eaa2855b78ad6b67e8695fe36c3590f
mikicvi <88291034+mikicvi@users.noreply.github.com>
2025-02-05 14:54:43 +0000

    feat: add GitHub Actions workflow for Docker image release and create production Docker Compose file

---
+++ release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Get username from env
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+
+# Define variables
+FRONTEND_IMAGE_NAME="$DOCKER_USERNAME/schedulrai-frontend"
+BACKEND_IMAGE_NAME="$DOCKER_USERNAME/schedulrai-backend"
+OLLAMA_IMAGE_NAME="$DOCKER_USERNAME/schedulrai-ollama"
+VERSION_TAG=$(date +%Y%m%d%H%M)
+
+# Create and use a new builder instance
+docker buildx create --use
+
+# Build and push frontend image for both x86 and ARM64
+echo "Building and pushing frontend image..."
+docker buildx build --platform linux/amd64,linux/arm64 -t $FRONTEND_IMAGE_NAME:latest -t $FRONTEND_IMAGE_NAME:$VERSION_TAG -f Dockerfile.frontend . --push
+
+# Build and push backend image for both x86 and ARM64
+echo "Building and pushing backend image..."
+docker buildx build --platform linux/amd64,linux/arm64 -t $BACKEND_IMAGE_NAME:latest -t $BACKEND_IMAGE_NAME:$VERSION_TAG -f Dockerfile.backend . --push
+
+# Build and push ollama image for both x86 and ARM64
+echo "Building and pushing ollama image..."
+docker buildx build --platform linux/amd64,linux/arm64 -t $OLLAMA_IMAGE_NAME:latest -t $OLLAMA_IMAGE_NAME:$VERSION_TAG -f Dockerfile.ollama . --push
+
+echo "Release completed successfully!"
